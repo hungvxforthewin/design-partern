0af9182f7bcee9561a607ba42c5def87b8cd2313
hungvx <vuxuanhungmta@gmail.com>
2021-10-12 17:12:43 +0700

    singleton mutil thread

--- design-pattern/singleton/MySingletonService.cs
+++ design-pattern/singleton/MySingletonService.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace singleton
 {
+    /// <summary>
+    /// Implement Singleton: Eager initialization
+    /// </summary>
     public class MySingletonService
     {
         private static int creationCount = 0;
@@ -17,16 +20,68 @@ namespace singleton
         private MySingletonService()
         {
             creationCount++;
+            Console.WriteLine("Counter Value " + creationCount.ToString());
         }
 
         //PUBLIC STATIC METHOD RETURN INSTANCE 
-        public static MySingletonService GetInstance() => _mySingletonServiceInstance;
+        public static  MySingletonService GetInstance() => _mySingletonServiceInstance;
 
+        //DISPLAY
         public int GetCreationCount() => creationCount;
 
         public void ViewCreationCount()
         {
             Console.WriteLine($"Count Instance {creationCount}");
+            Console.ReadKey();
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+    /// <summary>
+    /// Implement Singleton: Lazy initialization
+    /// </summary>
+    public class MySingletonService1
+    {
+        private static int creationCount1 = 0;
+
+        //PRIVATE ONLY 1 INSTANCE OF CLASS 
+        //private static MySingletonService1 _mySingletonServiceInstance1 = new MySingletonService1(); ??? WHY
+        private static MySingletonService1 _mySingletonServiceInstance1 = null;
+
+        //PRIVATE CONSTRUCTOR
+        private MySingletonService1()
+        {
+            creationCount1++;
+            Console.WriteLine("Counter Value " + creationCount1.ToString());
+
+        }
+
+        //PUBLIC STATIC METHOD RETURN INSTANCE 
+        public static MySingletonService1 GetInstance
+        {
+            get
+            {
+                if (_mySingletonServiceInstance1 == null)
+                {
+                    _mySingletonServiceInstance1 = new MySingletonService1();
+                }
+                return _mySingletonServiceInstance1;
+
+            }
+        }
+
+        public int GetCreationCount() => creationCount1;
+
+        public void ViewCreationCount()
+        {
+            Console.WriteLine($"Count Instance {creationCount1}");
+        }
+
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
         }
     }
 }
--- design-pattern/singleton/Program.cs
+++ design-pattern/singleton/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace singleton
 {
@@ -8,12 +10,28 @@ namespace singleton
         {
             Console.WriteLine("Hello World!");
             Display();
-            
+
+            //Singleton_Not_Synchronized();
+            //MySingletonService_Mutil_Thread();
+            MySingletonService_Mutil_Task();
+
+            // Parallel.Invoke(
+            //    () =>
+            //    PrintTeacherDetails(),
+            //    () =>
+            //    PrintStudentdetails()
+            //);
+        }
+
+        public static void Display() => Console.WriteLine("dotNET 5");
+
+        public static void Singleton_Not_Synchronized()
+        {
             //KHỞI TẠO INSTANCE 1
             MySingletonService serviceInstance1 = MySingletonService.GetInstance();
             //Console.WriteLine(serviceInstance1.GetCreationCount());
             serviceInstance1.ViewCreationCount();
-            
+
             //KHỞI TẠO INSTANCE 2
             MySingletonService serviceInstance2 = MySingletonService.GetInstance();
             //Console.WriteLine(serviceInstance2.GetCreationCount());
@@ -23,6 +41,107 @@ namespace singleton
             Console.WriteLine(serviceInstance1 == serviceInstance2);
         }
 
-        public static void Display() => Console.WriteLine("dotNET 5");
+        public static void MySingletonService_Mutil_Thread()
+        {
+            Thread t1 = new Thread(() =>
+            {
+                //KHỞI TẠO INSTANCE 1
+                MySingletonService serviceInstance1 = MySingletonService.GetInstance();
+                //Console.WriteLine(serviceInstance1.GetCreationCount());
+                serviceInstance1.ViewCreationCount();
+            });
+            Thread t2 = new Thread(() =>
+            {
+                //KHỞI TẠO INSTANCE 2
+                MySingletonService serviceInstance2 = MySingletonService.GetInstance();
+                //Console.WriteLine(serviceInstance2.GetCreationCount());
+                serviceInstance2.ViewCreationCount();
+            });
+            t1.Start();
+            t2.Start();
+
+
+            //KIỂM TRA CÁC INSTANCE ĐƯỢC TẠO
+            //Console.WriteLine(serviceInstance1 == serviceInstance2);
+        }
+
+        public static void MySingletonService_Mutil_Task()
+        {
+            Parallel.Invoke(
+               () =>
+               PrintTeacherDetails(),
+               () =>
+               PrintStudentdetails()
+           );
+
+
+            //KIỂM TRA CÁC INSTANCE ĐƯỢC TẠO
+            //Console.WriteLine(serviceInstance1 == serviceInstance2);
+        }
+        private static void PrintTeacherDetails()
+        {
+            MySingletonService1 serviceInstance1 = MySingletonService1.GetInstance;
+            serviceInstance1.PrintDetails("FROM A");
+        }
+        private static void PrintStudentdetails()
+        {
+            MySingletonService1 serviceInstance2 = MySingletonService1.GetInstance;
+            serviceInstance2.PrintDetails("FROM B");
+        }
     }
 }
+
+
+//namespace singleton
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Parallel.Invoke(
+//                () => PrintTeacherDetails(),
+//                () => PrintStudentdetails()
+//                );
+//            Console.ReadLine();
+//        }
+
+//        private static void PrintTeacherDetails()
+//        {
+//            Singleton fromTeacher = Singleton.GetInstance;
+//            fromTeacher.PrintDetails("From Teacher");
+//        }
+
+//        private static void PrintStudentdetails()
+//        {
+//            Singleton fromStudent = Singleton.GetInstance;
+//            fromStudent.PrintDetails("From Student");
+//        }
+//    }
+//}
+
+//namespace singleton
+//{
+//    public sealed class Singleton
+//    {
+//        private static int counter = 0;
+//        private static Singleton instance = null;
+//        public static Singleton GetInstance
+//        {
+//            get
+//            {
+//                if (instance == null)
+//                    instance = new Singleton();
+//                return instance;
+//            }
+//        }
+//        private Singleton()
+//        {
+//            counter++;
+//            Console.WriteLine("Counter Value " + counter.ToString());
+//        }
+//        public void PrintDetails(string message)
+//        {
+//            Console.WriteLine(message);
+//        }
+//    }
+//}
