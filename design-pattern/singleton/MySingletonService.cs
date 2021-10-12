10269e56fbcfb308d515771677bdd93b584ab9b5
hungvx <vuxuanhungmta@gmail.com>
2021-10-12 15:37:45 +0700

    singleton

---
+++ design-pattern/singleton/MySingletonService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singleton
+{
+    public class MySingletonService
+    {
+        private static int creationCount = 0;
+
+        //PRIVATE ONLY 1 INSTANCE OF CLASS 
+        private static readonly MySingletonService _mySingletonServiceInstance = new MySingletonService();
+
+        //PRIVATE CONSTRUCTOR
+        private MySingletonService()
+        {
+            creationCount++;
+        }
+
+        //PUBLIC STATIC METHOD RETURN INSTANCE 
+        public static MySingletonService GetInstance() => _mySingletonServiceInstance;
+
+        public int GetCreationCount() => creationCount;
+
+        public void ViewCreationCount()
+        {
+            Console.WriteLine($"Count Instance {creationCount}");
+        }
+    }
+}
---
+++ design-pattern/singleton/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Display();
+            
+            //KHỞI TẠO INSTANCE 1
+            MySingletonService serviceInstance1 = MySingletonService.GetInstance();
+            //Console.WriteLine(serviceInstance1.GetCreationCount());
+            serviceInstance1.ViewCreationCount();
+            
+            //KHỞI TẠO INSTANCE 2
+            MySingletonService serviceInstance2 = MySingletonService.GetInstance();
+            //Console.WriteLine(serviceInstance2.GetCreationCount());
+            serviceInstance2.ViewCreationCount();
+
+            //KIỂM TRA CÁC INSTANCE ĐƯỢC TẠO
+            Console.WriteLine(serviceInstance1 == serviceInstance2);
+        }
+
+        public static void Display() => Console.WriteLine("dotNET 5");
+    }
+}
